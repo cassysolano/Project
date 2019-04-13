f5607e323b4c794bbbb669b3f7b70f01b1ea8e86
cassysolano <48389829+cassysolano@users.noreply.github.com>
2019-04-13 10:58:15 -0500

    Add files via upload

--- birthday.py
+++ birthday.py
@@ -1,38 +1,35 @@
 '''
-Created on Apr 13, 2019
+Created on Mar 30, 2019
 
 @author: ITAUser
 '''
 import json
-answer='yes'
+answer = 'yes'
 while answer!='no':
-    with open("birthdays_1.json","r") as f_r:
+    with open (assignment.json, "r") as f_r:
         data = json.load(f_r)
-    
-    print("\n\nWe have birthdays of the following people...")
-    for i in data:
-        print ("\n" + i)
-    
-    ans = input("1. Find somebody's birthday. \n2. Add birthday \n\n")
-    if ans=='1':
-        name = input("Enter the person's name: ")
-        print ("The birthday for {} is {}".format(name, data.get(name, "not in our database")))
-     
-    elif ans=='2':
-        n = int(input("How many birthdays do you wanna add? (max 3 at a time) "))
-        i=0
-        while i<n:
-            print("\n Add Person ",i+1)
-            new_name = input("\nEnter the name: ")
-            new_birthday = input("Enter their birthday (dd/mm/yyyy):")
-            data[new_name] = new_birthday
-            with open("birthdays_1.json", "w") as f_w:
-                json.dump(data, f_w)
-            
-            print ("Birthday Added!")
+    print ("\n\n We have birthdays of the following people...")
+    for i in data
+        print("\n" + i)
         
-            i+=1
-    
+    ans= input("1. Find Math assignments. \m2 Find assignment by number \n\n")
+    if ans='1':
+        name = input("Enter math assignment name: ")
+        print ("Math for {} is {}" .format(name, data.get(name,"Not in our Database")))  
+    elif ans = '2':
+        n = int(input("Which assignment?"))
+        i = 0
+        while i<n:
+            print ("\b Add Person", i+1
+        new_name = input ("\n Enter the name:")
+        new_assignment = input ("\n Enter the number 1...10 :")
+        data:[new_name] = new_assignment  
+        with open("assignment.json", "w") as f_w:
+            json.dump(data, f_w)
+        print("Number of assignment")
+        i==1
+        
     else:
-        print("\nInvalid Choice!")
-    answer = input("\nDo you wanna use again (yes/no): ")
+        print("\n Invalid Choice")
+    answer = input ("\n Do you want use the assignment dictionary again (yes/no):")
+        
